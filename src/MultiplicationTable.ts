c5cc8d7482020ddb2f63d681af6c67e19202a59f
hockeyqqy <qqu@slb.com>
2020-11-17 16:35:28 +0800

    feat: pass test for case input(2,4)

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -3,10 +3,11 @@ export class MultiplicationTable {
   public render(start: number, end: number): string {
     const isValid = inputCheck(start, end)
     if (isValid) {
-      return 'success'
+      // return 'success'
+      const expressions: Expression[][] = buildExpressions(start, end)
+      return generateTable(expressions)
     }
     else return ''
-
   }
 }
 
@@ -31,6 +32,46 @@ function checkInRange(start: number, end: number): boolean {
 function checkOrder(start: number, end: number): boolean {
   return start < end
 }
+
+function buildExpressions(start: number, end: number): Expression[][] {
+  const expressions = [...Array(end - start + 1).keys()]
+  const a = expressions.map((row, rowIndex) => [...Array(rowIndex + 1).keys()].map(
+    (col, colIndex) => generateSingleExpression(start + colIndex, start + rowIndex)
+  )
+  )
+  // console.log(a)
+  return a
+  // console.log(a)
+
+  // return [[new Expression(2,4,8)]]
+
+}
+
+function generateSingleExpression(expressionStart: number, expressionEnd: number): Expression {
+  return new Expression(expressionStart, expressionEnd, expressionStart * expressionEnd)
+}
+
+
+function generateTable(expressions: Expression[][]): string {
+  // let outputexpression = [...Array(expressions.length).keys()]
+  //   .map((row, rowIndex) => [...Array(expressions[rowIndex].length).keys()])
+  // console.log(outputexpression)
+  const outputWithoutSpace = expressions.map((row) =>
+    row.map((col) => printSingleMultiplication(col)).join('  ')
+    // (col, colIndex) => printSingleMultiplication(expressions[rowIndex][colIndex])
+  ).join('\n')
+  console.log(outputWithoutSpace)
+  return outputWithoutSpace
+
+}
+
+function printSingleMultiplication(inputExpression: Expression): string {
+  // console.log(`${inputExpression.factor1}*${inputExpression.factor2}=${inputExpression.product}`)
+  return `${inputExpression.factor1}*${inputExpression.factor2}=${inputExpression.product}`
+}
+
+
+
 class Expression {
   constructor(factor1: number, factor2: number, product: number) {
     this.factor1 = factor1

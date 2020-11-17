5d4de0ea645a2e3d2e38ee355d938183e06e606b
hockeyqqy <qqu@slb.com>
2020-11-17 14:45:57 +0800

    feat: pass first test for invalid order

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,44 @@
+import { Console } from 'console'
+
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public render(start: number, end: number): string {
+    const isValid = inputCheck(start, end)
+    if (isValid) {
+      return 'success'
+    }
+    else return ''
+
   }
+}
+
+function inputCheck(start: number, end: number): boolean {
+  const isInOrder = checkOrder(start, end)
+  const isInRange = false
+  if (isInOrder) {
+    const isInRange = checkInRange(start, end)
+    if (!isInRange) {
+      console.log('Out of range.')
+    }
+    return isInRange
+  }
+  else {
+    return isInOrder
+  }
+
+}
+function checkInRange(start: number, end: number): boolean {
+  return (start > 0) && (end < 11)
+}
+function checkOrder(start: number, end: number): boolean {
+  return start < end
+}
+class Expression {
+  constructor(factor1: number, factor2: number, product: number) {
+    this.factor1 = factor1
+    this.factor2 = factor2
+    this.product = product
+  }
+  factor1: number
+  factor2: number
+  product: number
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,16 +1,28 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
+  // it('should render multiplication table of (1, 1)', () => {
+  //   // given
+  //   const table = new MultiplicationTable()
+  //   const start = 1
+  //   const end = 1
+
+  //   //when
+  //   const rendered = table.render(start, end)
+
+  //   //then
+  //   expect(rendered).toBe('1*1=1')
+  // })
+  it('should render return nothing when multiplication table of (3, 2)', () => {
     // given
     const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
+    const start = 3
+    const end = 2
 
     //when
     const rendered = table.render(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBe('')
   })
 })
